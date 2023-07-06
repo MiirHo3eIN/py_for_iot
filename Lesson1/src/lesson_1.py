9ea84f1ebe90070532bc3697540e097fb55b0564
MiirHo3eIN <amirhmoallemi@gmail.com>
2023-07-06 10:31:44 +0200

    Adding propagation of lesson 1

---
+++ Lesson1/src/lesson_1.py
@@ -0,0 +1,12 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+
+"""
+    This is a notebook for the first lesson of the course "Python for IoT data analysis"
+
+
+    The first lesson is the introduction to pandas to import and manipulate data
+
+    
+"""
